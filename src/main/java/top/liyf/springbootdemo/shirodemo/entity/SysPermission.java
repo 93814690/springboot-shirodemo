4e913e618c496be76df9c2b801e7de90aca9263b
yunfei <93814690@qq.com>
2018-10-15 10:18:36 +0800

    lombok，toString，exclude

--- src/main/java/top/liyf/springbootdemo/shirodemo/entity/SysPermission.java
+++ src/main/java/top/liyf/springbootdemo/shirodemo/entity/SysPermission.java
@@ -1,6 +1,7 @@
 package top.liyf.springbootdemo.shirodemo.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @Entity
 @Data
+@ToString(exclude = {"roles"})
 public class SysPermission implements Serializable {
     @Id
     @GeneratedValue
--- src/main/java/top/liyf/springbootdemo/shirodemo/entity/UserInfo.java
+++ src/main/java/top/liyf/springbootdemo/shirodemo/entity/UserInfo.java
@@ -35,14 +35,4 @@ public class UserInfo implements Serializable {
         return this.username + this.salt;
     }
 
-    @Override
-    public String toString() {
-        return "UserInfo{" +
-                "uid=" + uid +
-                ", username='" + username + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", salt='" + salt + '\'' +
-                '}';
-    }
 }
